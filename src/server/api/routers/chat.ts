52fbc29569b8f6fdd2268bc9de7ec17de1505cc2
nicssavard <105433845+nicssavard@users.noreply.github.com>
2023-08-07 07:08:04 +0800

    ratelimit 4 every hour

--- src/server/api/routers/chat.ts
+++ src/server/api/routers/chat.ts
@@ -6,6 +6,22 @@ import {
   OpenAIApi,
 } from "openai";
 
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
+
+// Create a new ratelimiter, that allows 10 requests per 10 seconds
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(4, "60 m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
+
 type Message = {
   role: "system" | "user" | "assistant";
   content: string;
@@ -28,6 +44,12 @@ export const chatRouter = createTRPCRouter({
       });
       if (!user) {
         return "No user found";
+      }
+
+      const { success } = await ratelimit.limit(input.userID);
+
+      if (!success) {
+        throw new Error("Rate limit exceeded");
       }
 
       let messages: Message[] = [
